985f356b0340d1c112a63bf6173117b7821c6745
Basti <51531217+crftwrk@users.noreply.github.com>
2023-10-14 22:42:38 +0200

    Add files via upload

---
+++ main.php
@@ -0,0 +1,22 @@
+<?php
+/*Plugin Name: bS Simple Notice
+Plugin URI: https://bootscore.me/
+Description: Plugin adds a simple alert to bootScore theme and does nothing more. It is a testing plugin for auto-updates.  
+Version: 1.0.0
+Tested up to: 6.3.2
+Requires PHP: 7.4
+Author: bootScore
+Author URI: https://bootscore.me
+License: MIT License
+*/
+
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+
+// Hook bs_after_primary content
+function my_function() {
+  echo '<div class=""><p class="alert alert-info">bS Simple Notice v1.0.0</p></div>';
+}
+add_action('bs_after_primary', 'my_function', 5);
