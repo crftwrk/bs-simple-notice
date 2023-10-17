f424f5a58788c2b6ad4f5c896d2ef5984e1b50f7
craftwerk <pst@crftwrk.de>
2023-10-17 16:13:02 +0200

    Update main.php

--- main.php
+++ main.php
@@ -2,13 +2,14 @@
 /*Plugin Name: bS Simple Notice
 Plugin URI: https://bootscore.me/
 Description: Plugin adds a simple alert to bootScore theme and does nothing more. It is a testing plugin for auto-updates.  
-Version: 1.1.0
+Version: 1.2.0
 Tested up to: 6.3.2
 Requires at least: 5.0
 Requires PHP: 7.4
 Author: bootScore
 Author URI: https://bootscore.me
 License: MIT License
+GitHub Plugin URI: https://github.com/crftwrk/bs-simple-notice
 */
 
 
@@ -19,6 +20,7 @@ defined( 'ABSPATH' ) || exit;
 /*
  * Update checker
  */
+/*
 require 'plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
@@ -33,13 +35,13 @@ $myUpdateChecker->setBranch('main');
 
 //Optional: If you're using a private repository, specify the access token like this:
 //$myUpdateChecker->setAuthentication('your-token-here');
-
+*/
 
 
 
 // Hook bs_after_primary content
 function my_function() {
-  echo '<div class=""><p class="alert alert-info">bS Simple Notice has been updated to v1.1.0</p></div>';
+  echo '<div class=""><p class="alert alert-info">bS Simple Notice has been updated to v1.2.0</p></div>';
 }
 add_action('bs_after_primary', 'my_function', 5);
 
