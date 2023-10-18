2f70372b758356548d4e9ae1f176a7066d6690a1
craftwerk <pst@crftwrk.de>
2023-10-18 18:54:43 +0200

    Add TukuToi updater

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 /*Plugin Name: bS Simple Notice
 Plugin URI: https://bootscore.me/
 Description: Plugin adds a simple alert to bootScore theme and does nothing more. It is a testing plugin for auto-updates.  
-Version: 1.3.0
+Version: 1.4.0
 Tested up to: 6.3.2
 Requires at least: 5.0
 Requires PHP: 7.4
@@ -16,9 +16,22 @@ License: MIT License
 defined( 'ABSPATH' ) || exit;
 
 
+
+/**
+ * Require the GitHub_Plugin_Updater class.
+ *
+ * Each of your plugins will require GitHub_Plugin_Updater to be loaded under a different namespace.
+ * Ideally use an autoloader, but for the sake of functionality we are using require_once here.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'class-github-plugin-updater.php';
+use MyPlugin\GitHub_Plugin_Updater;
+( new GitHub_Plugin_Updater( plugin_basename( __FILE__ ), '1.4.0', 'https://api.github.com/repos/crftwrk/bs-simple-notice' ) )->init();
+
+
 /*
  * Update checker
  */
+/*
 require 'plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
@@ -33,13 +46,13 @@ $myUpdateChecker->setBranch('main');
 
 //Optional: If you're using a private repository, specify the access token like this:
 //$myUpdateChecker->setAuthentication('your-token-here');
-
+*/
 
 
 
 // Hook bs_after_primary content
 function my_function() {
-  echo '<div class=""><p class="alert alert-info">bS Simple Notice has been updated to v1.3.0</p></div>';
+  echo '<div class=""><p class="alert alert-info">bS Simple Notice has been updated to v1.4.0</p></div>';
 }
 add_action('bs_after_primary', 'my_function', 5);
 
